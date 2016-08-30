4cf8c42b70eb221c02e5890a21c08b29203d7336
anish1024 <anishvijay1983@gmail.com>
2016-08-30 16:59:26 +0530

    s Contains the angular-quickstart tutorial project template

---
+++ angular-hero-webpack/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+   <h1>{{title}}</h1>
+   <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+      <router-outlet></router-outlet>
+   </nav>
+  `
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
